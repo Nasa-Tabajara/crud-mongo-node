83fe9d1b3fb2974941fdc98ecea39b1d981ffe3b
kyothiago <toberle2@hotmail.com>
2021-08-21 20:52:07 -0300

    finalizado o cliente!

--- app.js
+++ app.js
@@ -22,30 +22,39 @@ app.use('/clientes', cliente);
 app.use('/fornecedores', fornecedor);
 app.use('/vendas', venda);
 app.use('/produtos', produto);
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-  });
+});
+
 app.get('/comercio/clientes', function(req, res) {
     res.sendFile(path.join(__dirname, '/crudCliente.html'));
 }); 
+
 app.get('/comercio/fornecedores', function(req, res) {
     res.sendFile(path.join(__dirname, '/crudFornecedores.html'));
 }); 
+
 app.get('/comercio/produtos', function(req, res) {
     res.sendFile(path.join(__dirname, '/crudProdutos.html'));
 }); 
+
 app.get('/comercio/vendas', function(req, res) {
     res.sendFile(path.join(__dirname, '/crudVendas.html'));
 });
+
 app.get('/comercio/clientes/create', function(req, res) {
     res.sendFile(path.join(__dirname, './ecommerce/crudCliente/cadastro.html'));
 });
+
 app.get('/comercio/clientes/update', function(req, res) {
     res.sendFile(path.join(__dirname, './ecommerce/crudCliente/atualiza.html'));
 });
+
 app.get('/comercio/clientes/delete', function(req, res) {
     res.sendFile(path.join(__dirname, './ecommerce/crudCliente/remove.html'));
 });
+
 app.get('/comercio/clientes/details', function(req, res) {
     res.sendFile(path.join(__dirname, './ecommerce/crudCliente/consulta.html'));
 });
